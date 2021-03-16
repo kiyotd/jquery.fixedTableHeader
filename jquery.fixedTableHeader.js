94a7a6bec1fc8fe7c4df8b1e80601b779110ef6c
kiyotd <torigoedesign+kiyotd@gmail.com>
2021-03-16 13:47:50 +0900

    init

---
+++ jquery.fixedTableHeader.js
@@ -0,0 +1,29 @@
+/**
+ * Fixed Table Header Plugin for jQuery
+ * 
+ * @version 1.0.0
+ * @license MIT
+ * @author Kiyofumi Torigoe (kiyotd, torigoedesign)
+ * @copyright 2021 Kiyofumi Torigoe
+ */
+(function ($) {
+
+  const defaultTarget = "th";
+
+  const defaultCss = {
+    position: "sticky",
+    top: 0
+  };
+
+  $.fn.fixedTableHeader = function (target = null, css = null) {
+
+    target = target ?? defaultTarget;
+    css = css ?? defaultCss;
+
+    const resultCss = Object.assign({}, defaultCss, css)
+    // const resultCss = { ...defaultCss, ...css }
+
+    $(target, this).css(resultCss);
+
+  }
+})(jQuery);
